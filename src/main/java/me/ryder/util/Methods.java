2febd1949c2aed675bc4ec9a3d7f9346884d1f15
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2019-07-01 01:32:56 -0400

    Add files via upload

---
+++ src/main/java/me/ryder/util/Methods.java
@@ -0,0 +1,9 @@
+package me.ryder.util;
+
+import org.bukkit.ChatColor;
+
+public class Methods {
+    public String C(String M) {
+        return ChatColor.translateAlternateColorCodes('&', M);
+    }
+}
